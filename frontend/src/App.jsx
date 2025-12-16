649b4a6a9ce629f7e666153f6faa5e3a6d0a7782
hzaid01 <zaidsohail555@gmail.com>
2025-12-16 22:09:54 +0500

    commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -24,7 +24,7 @@ function App() {
   }
 
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
